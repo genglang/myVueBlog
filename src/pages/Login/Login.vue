6982dce9bd00ab2690c1564184bfeab5fb421011
genglang <657578302@qq.com>
2018-08-28 23:46:33 +0800

    '构建文件路径'

---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="LoginView">
+
+    </div>
+</template>
+
+<script type="es6">
+	export default {
+		name: 'Login',
+		data () {
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
