8fd21114fb80cf8e84cac83b0e01e79a5a4e5bee
genglang <657578302@qq.com>
2018-08-28 22:34:02 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        {{mingName}}
+        <div>{{doSth}}</div>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+		data () {
+			return {
+				mingName: '明老板',
+				doSth: '嫖娼萝莉被抓了'
+			}
+		}
+	}
+</script>
+
+
+<style lang='scss' scoped>
+    #app {
+        width: 100vw;
+        height: 100vh;
+        background-color: red;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 48px;
+    }
+</style>
