7b2d85fa13f28d154f0422d4c1d904524b20ff0e
genglang <657578302@qq.com>
2018-09-11 22:21:23 +0800

    '添加正文页面'

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<header>页头</header>
+		<main>正文</main>
+		<footer>页尾</footer>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home'
+	}
+</script>
+
+<style scoped>
+
+</style>
