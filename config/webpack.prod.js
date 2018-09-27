9dc7da9f85e45fc6cae237377e87c723c37c9f31
genglang <657578302@qq.com>
2018-09-27 23:44:36 +0800

    '添加打包前清除dist文件夹成功'

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,10 @@
 const glob = require('glob')
 const path = require('path')
 const merge = require('webpack-merge')
-const uglify = require('uglifyjs-webpack-plugin') // js压缩
+const Uglify = require('uglifyjs-webpack-plugin') // js压缩
 const PurifyCSSPlugin = require('purifycss-webpack')
 const common = require('./webpack.common.js')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
 	mode: 'production',
@@ -12,7 +13,11 @@ module.exports = merge(common, {
 		main: './src/main.js',
 	},
 	plugins: [
-		new uglify(),
+		new CleanWebpackPlugin(['dist'], {
+			root: path.resolve(__dirname, '../'),
+			verbose: true
+		}),
+		new Uglify(),
 		new PurifyCSSPlugin({
 			// 这里配置了一个paths，主要是需找html模板，purifycss根据这个配置会遍历你的文件，查找哪些css被使用了。
 			// 注意： 使用这个插件必须配合extract-text-webpack-plugin这个插件
